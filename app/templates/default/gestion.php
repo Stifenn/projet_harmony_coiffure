4c8d0bc289a1a376f271b8d2d55c0aa6e7cc3b7a
Stifenn <stifenn@gmail.com>
2016-01-15 09:34:28 +0100

    création de la page gestion
    fichier d'accueil du back

---
+++ app/templates/default/gestion.php
@@ -0,0 +1,9 @@
+<?php $this->layout('layoutBack', ['title' => 'Gestion']) ?>
+
+<?php $this->start('main_content') ?>
+	<h2>Let's code.</h2>
+	
+	<a href="<?= $this->url('slider') ?>">Slider</a><br>
+	<a href="<?= $this->url('lookbook') ?>">Lookbook</a>
+	
+<?php $this->stop('main_content') ?>
