52ac42781083a26866377e4d86272a69b40ab0df
Benoit Gaschen <bens.57@hotmail.fr>
2016-01-15 09:49:13 +0100

    Revert "jour 2"
    This reverts commit 41c512f5b01bd8b15e59007602de149ecc541804.

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,5 @@
 <?php
 	
 	$w_routes = array(
-		['GET', 		'/', 				'Default#home', 	'home'],
-		['GET|POST', 	'/login', 			'Default#login', 	'login'],
-		['GET', 		'/logoff', 			'Default#logoff', 	'logoff'],
-		['GET', 		'/gestion', 		'Default#gestion', 	'gestion'],
-
-		['GET|POST', 	'/creation/compte', 'User#create_user', 'create_user'],
+		['GET', '/', 'Default#home', 'home'],
 	);
--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -1,30 +1,6 @@
 <?php $this->layout('layout', ['title' => 'Accueil !']) ?>
 
 <?php $this->start('main_content') ?>
-
-	<h2>Accueil Front</h2>
-
-<?php 
-	// Si il n'y a pas de session active j'affiche le lien de connexion
-	if (!isset($_SESSION['user'])) : ?>
-		<a href="<?= $this->url('login') ?>">Se connecter</a> | 
-		<a href="<?= $this->url('create_user') ?>">Créer un compte</a>
-<?php endif; ?>
-
-<?php
-	// si la session est active
-	if(isset($_SESSION['user'])) :
-		// on affiche le nom et l'email de l'utilisateur ?>
-		<div id="user">
-			<?= $_SESSION['user']['nom'] ?> (<em><?= $_SESSION['user']['email'] ?></em>)<br>
-			<?php
-				// si on est admin on affiche un lien vers la gestion
-				if ($_SESSION['user']['role'] == 'admin') : ?>
-					<a href="<?= $this->url('gestion') ?>">Gestion</a>
-				<?php endif;
-				// si on est connecté on propose un lien de déconnexion ?>
-			<a href="<?= $this->url('logoff') ?>">Déconnexion</a>
-		</div>
-<?php endif; ?>
-
+	<h2>Let's code.</h2>
+	
 <?php $this->stop('main_content') ?>
