a11d90e08b0d9ab84995e6a8d3ceaf2af124c8ad
Stifenn <stifenn@gmail.com>
2016-01-28 16:57:28 +0100

    commit jour 11 aprem

--- app/config.php
+++ app/config.php
@@ -7,7 +7,7 @@ $w_config = [
    	//information de connexion à la bdd
 	'db_host' => 'localhost',						//hôte (ip, domaine) de la bdd
     'db_user' => 'root',							//nom d'utilisateur pour la bdd
-    'db_pass' => 'azertypoiu',								//mot de passe de la bdd
+    'db_pass' => 'webforce3',								//mot de passe de la bdd
     'db_name' => 'projet_harmony_coiffure',								//nom de la bdd
     'db_table_prefix' => '',						//préfixe ajouté aux noms de table
 
--- app/templates/commentaires/commentaires.php
+++ app/templates/commentaires/commentaires.php
@@ -1,13 +1,13 @@
 <?php $this->layout('layoutBack', ['title' => 'Gestion des commentaires']) ?>
 
 <?php $this->start('main_content') ?>
-<div class="row">
+<div class="row" >
   	<div class="col-md-6 col-md-offset-3">
 		<?php foreach($Commentaires as $CurrentCommentaires) : ?>
 			<article>
-				<p><?= $CurrentCommentaires['pseudo'] ?></p>
-				<p><?= $CurrentCommentaires['email'] ?></p>
-				<p><?= $CurrentCommentaires['commentaire'] ?></p>
+				<p>Pseudo : <?= $CurrentCommentaires['pseudo'] ?></p>
+				<p>Email : <?= $CurrentCommentaires['email'] ?></p>
+				<p>Message : <?= $CurrentCommentaires['commentaire'] ?></p>
 			</article>
 			<aside>
 				<form action="<?= $this->url('modifier_commentaires',['id'=>$CurrentCommentaires['id']])?>" method="POST" accept-charset="utf-8">
